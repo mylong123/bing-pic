e923305c8ebdf696130dec1651e74bdcdbbc9ce6
mylong123 <1302070253@qq.com>
2020-05-31 00:21:23 +0800

    11
    1212

--- spider.py
+++ spider.py
@@ -7,11 +7,11 @@ def GetPic():
     json_data = json.loads(api.text)
     pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
     start_date = json_data['images'][0]['startdate']
-    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+    open(r'./tmp/json/{0}.json'.format(start_date), 'wb').write(api.content)
     print('Create Json Success!')
     pic = get(pic_url, stream=True)
     if(pic.status_code == 200):
-        open(r'./pic/{0}.png'.format(start_date), 'wb').write(pic.content)
+        open(r'./tmp/pic/{0}.png'.format(start_date), 'wb').write(pic.content)
         print('Create Image Success!')
     else:
         print('Create Image Faild!')
